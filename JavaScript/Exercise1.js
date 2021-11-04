27843a7bef561203040d50007afc87324b4ed9f0
Alekya Dandu <alekya.dandu@klh.edu.in>
2021-11-04 13:12:25 +0530

    Finished book exercise 1

--- JavaScript/Exercise1.js
+++ JavaScript/Exercise1.js
@@ -4,6 +4,9 @@ function book(title, author,noofpages,status){
     this.noofpages=noofpages;
     this.status=status;
     this.info = function(){
-        return tittle+" by "+author+", "+noofpages+" pages, "+ status;
+        return title+" by "+author+", "+noofpages+" pages, "+ status;
     }
 }
+
+const book1 = new book("The Hobbit","J.R.R Tolien",295, "has read");
+console.log(book1.info());
